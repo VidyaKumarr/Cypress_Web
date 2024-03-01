8f790e246136d7b3faa80afb47e718d4b6c43d9f
VidyaKumarr <vidyakumarr@gmail.com>
2024-03-01 23:41:14 +0530

    ci/cd for artifacts2

--- cypress/e2e/pages/loginPage.js
+++ cypress/e2e/pages/loginPage.js
@@ -8,7 +8,7 @@ login()
 
     cy.visit('')
     cy.printTitle()
-    cy.get('#login').click()
+    //cy.get('#login').click()
     //cy.readFile("C:\Users\vidya\OneDrive\Desktop\Web\Cypress_Web\cypress\screenshots/login.feature/I want to login into the site -- Login Test Case (failed).png")
                  //C:\Users\vidya\OneDrive\Desktop\Web\Cypress_Web\cypress\screenshots\login.feature\I want to login into the site -- Login Test Case (failed).png
  }
