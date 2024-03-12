d3a4d2976f31ea3566b79db0f1ed86cc86509ef2
Anshita Bhasin <anshita.bhasin@gmail.com>
2024-03-12 22:29:15 +0400

    Web Table - Print all rows and columns

--- cypress/e2e/pages/loginPage.js
+++ cypress/e2e/pages/loginPage.js
@@ -7,18 +7,45 @@ export class loginPage {
 
         cy.visit('')
         cy.printTitle()
-
-        //cy.readFile("/Users/anshita/Desktop/anshita/Cypress_Web/cypress/screenshots/login.feature/I want to login into the site -- Login Test Case (failed).png") 
-
-
-
-        //cy.log("**folder name**", Cypress.config("screenshotsFolder")+/`${Cypress.spec.name}`+/`${testState.feature.name} -- ${testState.currentScenario.name} (failed).png`)
-
-
         cy.get('#login').click()
 
 
         //cy.readFile("C:\Users\vidya\OneDrive\Desktop\Web\Cypress_Web\cypress\screenshots/login.feature/I want to login into the site -- Login Test Case (failed).png")
         //C:\Users\vidya\OneDrive\Desktop\Web\Cypress_Web\cypress\screenshots\login.feature\I want to login into the site -- Login Test Case (failed).png
     }
+
+
+
+
+    tablehandle() {
+        cy.visit('https://testautomationpractice.blogspot.com/');
+        cy.get('table[name="BookTable"] > tbody > tr').contains('td', 'Selenium').should('be.visible');
+        cy.get('table[name="BookTable"] > tbody > tr > td').first().then($value => {
+            const textValue = $value.text();
+            cy.wrap(textValue).as('wrapValue');
+        });
+    }
+
+    validateTableText() {
+        cy.get('@wrapValue').then((wrapValue) => {
+            cy.log('** print text **' + wrapValue);
+        });
+    }
+
+
+    readAllRowsAndCols() {
+        cy.get('table[name="BookTable"]').find('tbody > tr').each(($row, index, $rows) => {
+
+            if (index > 0) {
+                cy.wrap($row).within(() => {
+                    cy.get('td').each(($col, index, $cols) => {
+                        cy.log($col.text());
+                    });
+                });
+            }
+        });
+    }
+
+
 }
+
